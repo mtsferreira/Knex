1ae5ab0ef5dafcdda2b1b6b6fb01f4758d8ad7a1
Mtsferreira <mateustutulilu@gmail.com>
2020-09-13 16:19:11 -0300

    KnexCRUD

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    // informações q ja vem prontas por padrão ( Ex: usuário admin )
+    // seeds: {
+    //   directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    // },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ src/controllers/UserController.js
@@ -0,0 +1,40 @@
+const knex = require('../database/connection');
+
+module.exports = {
+
+    // retorna a tabela de usuários
+    async index (request, response) {
+        const results = await knex('users')
+
+        return response.json(results)
+    },
+
+    // cria um usuáio
+    async create(request, response) {
+        const { username } = request.body;
+
+        await knex('users').insert({
+            username
+        })
+        return response.status(201).send()
+    },
+
+    // atualiza o usuário
+    async update(request, response) {
+        const { username } = request.body;
+        const { id } = request.params;
+
+        await knex('users').update({ username }).where({ id })
+
+        return response.send()
+    },
+
+    // deletar um usuário
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex('users').where({ id }).del()
+
+        return response.send()
+    },
+}
